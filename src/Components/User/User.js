5aa52bd6beb33fea79b3c5ff3516cdb0ac6b26d1
wallisonBatista <wallisongbatista@gmail.com>
2021-06-06 14:10:11 -0300

    styled page account and added icons

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
+import Feed from '../Feed/Feed';
+import UserHeader from './UserHeader';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
+
 
 const User = () => {
     return (
-        <div>
-            usuário
-        </div>
+        <section className="container">
+            <UserHeader />
+            <Routes>
+                <Route path="/" element={<Feed />} />
+                <Route path="postar" element={<UserPhotoPost />} />
+                <Route path="estatistica" element={<UserStats />} />
+            </Routes>
+        </section>
     )
 }
 
