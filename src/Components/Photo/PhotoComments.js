7faad81b4f9dd5420c5230c82ab022c1580009f3
wallisonBatista <wallisongbatista@gmail.com>
2021-06-12 00:44:31 -0300

    stylized comments component

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import styles from './PhotoComments.module.css';
 
-export const PhotoComments = () => {
-  return <div></div>;
+export const PhotoComments = (props) => {
+  const { login } = React.useContext(UserContext);
+  const commentsSection = React.useRef(null);
+  const [comments, setComments] = React.useState(() => props.comments);
+
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
+
+  return (
+    <>
+      <ul ref={commentsSection} class={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
