b79e9dbe48c9de5a6a1c901fd95f812c3c4b0a04
wallisonBatista <wallisongbatista@gmail.com>
2021-06-12 21:45:36 -0300

    created skeleton to photos

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -4,6 +4,7 @@ import { UserContext } from '../../UserContext';
 import { PhotoComments } from './PhotoComments';
 import styles from './PhotoContent.module.css';
 import PhotoDelete from './PhotoDelete';
+import Image from '../Helper/Image';
 
 const PhotoContent = ({ data }) => {
   const { photo, comments } = data;
@@ -12,7 +13,7 @@ const PhotoContent = ({ data }) => {
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
