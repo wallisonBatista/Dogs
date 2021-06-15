4e2c74ceea2677e94984ca6b260a085aae04473c
wallisonBatista <wallisongbatista@gmail.com>
2021-06-15 15:31:31 -0300

    created loading animated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Feed from './Feed/Feed';
+import Loading from './Helper/Loading';
 
 const Home = () => {
-    return (
-        <section className="container mainContainer">
-            <Feed />
-        </section>
-    )
-}
+  return (
+    <section className="container mainContainer">
+      <Feed />
+    </section>
+  );
+};
 
 export default Home;
